cc3a0f68a942b737f73c6c4c4c94961e0094c79f
Suresh <vuppala.sureshbabu@cognizant.com>
2025-02-20 21:12:44 -0500

    updtaed

---
+++ terraform/main.tf
@@ -0,0 +1,67 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# ECS Cluster
+resource "aws_ecs_cluster" "flask_cluster" {
+  name = "flask-cluster"
+}
+
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# ECS Task Definition
+resource "aws_ecs_task_definition" "flask_task" {
+  family                   = "flask-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = "512"
+  cpu                      = "256"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "flask-container"
+    image     = "${var.ecr_repo_url}:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 5000
+      hostPort      = 5000
+    }]
+  }])
+}
+
+# ECS Service
+resource "aws_ecs_service" "flask_service" {
+  name            = "flask-service"
+  cluster         = aws_ecs_cluster.flask_cluster.id
+  task_definition = aws_ecs_task_definition.flask_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = aws_subnet.public[*].id
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+}
+
+# Create an ECR Repository
+resource "aws_ecr_repository" "my_ecr" {
+  name = "my-flask-app"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,3 @@
+variable "ecr_repo_url" {
+  default = "AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/my-flask-app"
+}
