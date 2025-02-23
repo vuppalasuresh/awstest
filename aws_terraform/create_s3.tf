a7b4c977dda39cd7b86551937d28d2ca05391f94
shaikk-arch <shaikk.afreen@gmail.com>
2025-02-23 00:21:50 -0500

    test s3

--- aws_terraform/create_s3.tf
+++ aws_terraform/create_s3.tf
@@ -1,31 +1,31 @@
-provider "aws" {
-  region = "us-east-1"
-}
+# provider "aws" {
+#   region = "us-east-1"
+# }
 
-resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "my-9898989-123-secure-bucket-123456"
-}
+# resource "aws_s3_bucket" "secure_bucket" {
+#   bucket = "my-9898989-123-secure-bucket-123456"
+# }
 
-resource "aws_s3_bucket_public_access_block" "block_public_access" {
-  bucket = aws_s3_bucket.secure_bucket.id
+# resource "aws_s3_bucket_public_access_block" "block_public_access" {
+#   bucket = aws_s3_bucket.secure_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-  bucket = aws_s3_bucket.secure_bucket.id
+# resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
+#   bucket = aws_s3_bucket.secure_bucket.id
 
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
+#   rule {
+#     apply_server_side_encryption_by_default {
+#       sse_algorithm = "AES256"
+#     }
+#   }
+# }
 
-# Data source to validate bucket in test script
-data "aws_s3_bucket" "secure_bucket" {
-  bucket = aws_s3_bucket.secure_bucket.id
-}
+# # Data source to validate bucket in test script
+# data "aws_s3_bucket" "secure_bucket" {
+#   bucket = aws_s3_bucket.secure_bucket.id
+# }
