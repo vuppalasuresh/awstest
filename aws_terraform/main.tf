436de03cdbd431a6156e8d6dfb0a8df3b9df9eab
shaikk-arch <shaikk.afreen@gmail.com>
2025-02-23 00:00:38 -0500

    test s3

--- aws_terraform/main.tf
+++ aws_terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "secure_bucket" {
-  bucket = "my-12341234564-secure-bucket-123456"
+  bucket = "my-9898989secure-bucket-123456"
 }
 
 resource "aws_s3_bucket_acl" "private_acl" {
@@ -21,24 +21,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
   }
 }
 
-# Automated Tests
-test {
-  check "bucket_exists" {
-    assert {
-      condition     = length(data.aws_s3_bucket.secure_bucket.id) > 0
-      error_message = "S3 bucket does not exist."
-    }
-  }
-
-  check "encryption_enabled" {
-    assert {
-      condition     = data.aws_s3_bucket.secure_bucket.server_side_encryption_configuration[0].rule[0].apply_server_side_encryption_by_default.sse_algorithm == "AES256"
-      error_message = "S3 bucket encryption is not enabled or not using AES256."
-    }
-  }
-}
-
-# Data source to fetch bucket details for testing
+# Data source to validate bucket in test script
 data "aws_s3_bucket" "secure_bucket" {
   bucket = aws_s3_bucket.secure_bucket.id
 }
---
+++ tests/test.tf
@@ -0,0 +1,18 @@
+check "bucket_exists" {
+  assert {
+    condition     = length(data.aws_s3_bucket.secure_bucket.id) > 0
+    error_message = "S3 bucket does not exist."
+  }
+}
+
+check "encryption_enabled" {
+  assert {
+    condition     = data.aws_s3_bucket.secure_bucket.server_side_encryption_configuration[0].rule[0].apply_server_side_encryption_by_default.sse_algorithm == "AES256"
+    error_message = "S3 bucket encryption is not enabled or not using AES256."
+  }
+}
+
+# Data source to fetch bucket details for validation
+data "aws_s3_bucket" "secure_bucket" {
+  bucket = "my-9898989secure-bucket-123456"
+}
